515f9c1672cec6823abe68735a9c07da969bf662
Uladzislau Ulasovich <ulasowitch.vlad@gmail.com>
2023-10-26 16:03:19 +0200

    Open file & print its content to text field

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,30 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.TextStyle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.awt.FileDialog
+import java.awt.Frame
+import java.io.BufferedReader
+import java.io.FileReader
 
 @Composable
 fun BigDebugApp() {
     var textValue by remember { mutableStateOf("") }
-    var button1Clicked by remember { mutableStateOf(false) }
     var button2Clicked by remember { mutableStateOf(false) }
+
+    var selectedFileContent by remember { mutableStateOf<String?>(null) }
 
     MaterialTheme {
         Box(modifier = Modifier.fillMaxSize()) {
@@ -27,17 +35,24 @@ fun BigDebugApp() {
                 ) {
                     Button(
                         onClick = {
-                            button1Clicked = true
-                            button2Clicked = false
+                            val fileDialog = FileDialog(Frame(), "Select a file", FileDialog.LOAD)
+                            fileDialog.isVisible = true
+                            val selectedFile = fileDialog.file
+                            val selectedFilePath = fileDialog.directory + selectedFile
+                            if (selectedFile != null) {
+                                loadFileContent(selectedFilePath) { content ->
+                                    selectedFileContent = content
+                                    textValue = content
+                                }
+                            }
                         },
                         modifier = Modifier.fillMaxWidth().padding(16.dp)
                     ) {
-                        Text("Button 1")
+                        Text("Open file")
                     }
 
                     Button(
                         onClick = {
-                            button1Clicked = false
                             button2Clicked = true
                         },
                         modifier = Modifier.fillMaxWidth().padding(16.dp)
@@ -50,12 +65,18 @@ fun BigDebugApp() {
                 Column(
                     modifier = Modifier.fillMaxSize().background(Color.Black)
                 ) {
-                    BasicTextField(
-                        value = textValue,
-                        onValueChange = { textValue = it },
-                        modifier = Modifier.fillMaxSize(),
-                        textStyle = TextStyle(color = Color.White)
-                    )
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        BasicTextField(
+                            value = selectedFileContent ?: textValue,
+                            onValueChange = {
+                                selectedFileContent = it
+                                textValue = it
+                            },
+                            textStyle = TextStyle(color = Color.White)
+                        )
+                    }
                 }
             }
         }
@@ -63,7 +84,22 @@ fun BigDebugApp() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Kotlin Compose Debug App"
+    ) {
         BigDebugApp()
+    }
+}
+
+fun loadFileContent(filePath: String, callback: (String) -> Unit) {
+    runBlocking {
+        launch(Dispatchers.IO) {
+            val file = FileReader(filePath)
+            val reader = BufferedReader(file)
+            val content = reader.readText()
+            reader.close()
+            callback(content)
+        }
     }
 }
